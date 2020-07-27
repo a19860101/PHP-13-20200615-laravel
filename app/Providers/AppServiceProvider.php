b31379f5dc5b83d6cb2dbaebcff731619257cad4
Zac <a19860101@gmail.com>
2020-07-27 21:19:40 +0800

    20200727

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Schema::defaulStringLength(191);
+        Schema::defaultStringLength(191);
     }
 }
