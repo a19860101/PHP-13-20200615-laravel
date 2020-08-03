fbd61f52f3980e8cb2709109cf9d7f078a4caefe
Zac <a19860101@gmail.com>
2020-08-03 21:54:05 +0800

    20200803

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -131,7 +132,7 @@ class PostController extends Controller
         //
         // $post = Post::findOrFail($post->id);
         // $post->delete();
-
+        Storage::delete('public/images/'.$post->cover);
         Post::destroy($post->id);
 
         return redirect('post');
