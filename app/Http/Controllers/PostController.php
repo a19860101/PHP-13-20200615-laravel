e312e1197f4a9cc689d9590a5f57a4c45f12d1fc
Zac <a19860101@gmail.com>
2020-08-03 19:20:22 +0800

    20200803

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -87,6 +87,12 @@ class PostController extends Controller
     public function update(Request $request, $id)
     {
         //
+        DB::table('posts')->where('id',$id)->update([
+            'title'     => $request->title,
+            'content'   => $request->content,
+            'updated_at'=> now()
+        ]);
+        return redirect('post');
     }
 
     /**
