dfdb3c53bc037c46bcf9148d6c563761080301ff
Zac <a19860101@gmail.com>
2020-08-05 21:37:24 +0800

    20200805

--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     //建立白名單
     protected $fillable = ['title','content'];
+
+    use SoftDeletes;
 }
