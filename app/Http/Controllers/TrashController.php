f367cda2b1fd1c11c75d410a93b7aafaa5b6f264
Zac <a19860101@gmail.com>
2020-08-05 21:58:56 +0800

    20200805

--- app/Http/Controllers/TrashController.php
+++ app/Http/Controllers/TrashController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class TrashController extends Controller
 {
     //
     public function index(){
-        return 'trash';
+        $posts = Post::onlyTrashed()->get();
+        return $posts;
     }
 }
