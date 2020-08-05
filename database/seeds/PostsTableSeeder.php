655464caf8344d63a5fdb9b79bf133cf16da6a52
Zac <a19860101@gmail.com>
2020-08-05 19:23:03 +0800

    20200805

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,20 +12,24 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         //
-        Post::create([
-            'title'=>'標題一',
-            'content'=>'內容一',
-            'cover'=>'https://picsum.photos/id/15/800/600'
-        ]);
-        Post::create([
-            'title'=>'標題2',
-            'content'=>'內容2',
-            'cover'=>'https://picsum.photos/id/12/800/600'
-        ]);
-        Post::create([
-            'title'=>'標題3',
-            'content'=>'內容3',
-            'cover'=>'https://picsum.photos/id/14/800/600'
-        ]);
+
+        Post::truncate();
+        for($i=0;$i<10;$i++){
+            Post::create([
+                'title'=>'標題'.$i,
+                'content'=>'內容'.$i,
+                'cover'=>'https://picsum.photos/id/'.$i.'/800/600'
+            ]);
+        }
+        // Post::create([
+        //     'title'=>'標題2',
+        //     'content'=>'內容2',
+        //     'cover'=>'https://picsum.photos/id/12/800/600'
+        // ]);
+        // Post::create([
+        //     'title'=>'標題3',
+        //     'content'=>'內容3',
+        //     'cover'=>'https://picsum.photos/id/14/800/600'
+        // ]);
     }
 }
