4157367d9f7a340df4c3f3857515e75fcefc9d96
Zac <a19860101@gmail.com>
2020-08-17 19:55:37 +0800

    20200817

--- app/Post.php
+++ app/Post.php
@@ -20,4 +20,7 @@ class Post extends Model
     public function cate(){
         return $this->belongsTo('App\Cate');
     }
+    public function tags(){
+        return $this->belongsToMany('App\Tag');
+    }
 }
